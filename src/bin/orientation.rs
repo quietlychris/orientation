b50419bc3af181695429e575de041f357ca23ac5
chrism <christopher.and.moran@gmail.com>
2022-09-25 16:03:57 -0400

    Major refactor, update to Bevy 0.8

--- src/bin/orientation.rs
+++ src/bin/orientation.rs
@@ -9,7 +9,9 @@ pub struct Node<T: Message>(pub MeadowNode<Subscription, T>);
 #[derive(Component, Default)]
 struct Cube;
 
-pub const MEADOW_ADDR: &str = "192.168.8.105:25000";
+// pub const HOST_ADDR: &str = "192.168.8.121:25000";
+pub const HOST_ADDR: &str = "127.0.0.1:25000";
+
 
 fn main() {
     App::new()
@@ -22,7 +24,7 @@ fn main() {
         .add_startup_system(meadow_host)
         .add_startup_system(imu_recv_node)
         .add_system(rotate_cube)
-        .add_system(bevy::input::system::exit_on_esc_system)
+        .add_system(bevy::window::close_on_esc)
         .run();
 }
 
@@ -62,7 +64,7 @@ fn setup(
 
     commands.spawn_bundle(cube).insert(Cube);
 
-    commands.spawn_bundle(PerspectiveCameraBundle {
+    commands.spawn_bundle(Camera3dBundle {
         transform: Transform::from_xyz(6.0, 0.0, 0.0)
             .looking_at(Vec3::new(0.0, 0.0, 0.0), Vec3::new(0.0, 0.0, 1.0)),
         ..Default::default()
@@ -77,17 +79,17 @@ fn setup(
 
 fn rotate_cube(
     // time: Res<Time>,
-    mut query: Query<(&mut Cube, &mut Transform, &mut GlobalTransform)>,
+    mut query: Query<(&Cube, &mut Transform)>,
     mut imu_node_query: Query<&mut Node<Quat>>,
 ) {
     // Since we're directly reading/writing the GlobalTransform, we don't need
     // the timestep; otherwise, this would be useful
-    // let delta = time.delta_seconds();
-    let (_cube, _transform, mut global_transform) = query.single_mut();
+    let (cube, mut transform) = query.single_mut();
     let imu_recv_node = imu_node_query.single_mut();
 
+    // let (mut scale, mut rotation, mut translation): (Vec3, Quat, Vec3);
     if let Ok(quat) = imu_recv_node.0.get_subscribed_data() {
-        global_transform.rotation = quat;
+        transform.rotation = quat;
     }
 }
 
@@ -95,7 +97,8 @@ fn meadow_host(mut commands: Commands) {
     // Setup our meadow host on WiFi
     // The network interface of your WiFi adapter may be different; use `ifconfig` to check
     let meadow_host: MeadowHost = HostConfig::default()
-        .with_udp_config(Some(host::UdpConfig::default("wlp5s0")))
+        .with_udp_config(Some(host::UdpConfig::default("wlp166s0")))
+        .with_tcp_config(Some(host::TcpConfig::default("lo")))
         .build()
         .expect("Couldn't create a Host");
 
@@ -109,11 +112,13 @@ fn meadow_host(mut commands: Commands) {
 fn imu_recv_node(mut commands: Commands) {
     // Sleep for a second while setting up to allow the Host to fully get setup
     std::thread::sleep(std::time::Duration::from_millis(500));
+    let host_addr = HOST_ADDR.parse::<std::net::SocketAddr>().unwrap();
     let meadow_node = NodeConfig::<Quat>::new("IMU_SUBSCRIPTION")
+        .with_tcp_config(Some(node::TcpConfig::default().set_host_addr(host_addr)))
         .topic("quaternion")
         .build()
         .unwrap()
-        .subscribe(std::time::Duration::from_millis(50)) // Run subscriber at 20 Hz
+        .subscribe(std::time::Duration::from_millis(10)) // Run subscriber at 20 Hz
         .unwrap();
     // Wrap our meadow node in the NewType pattern for Bevy
     let imu_node = Node(meadow_node);
